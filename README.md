77e0ce22b59b3c46773a79463d11c28e72e5a80b
Johanna Räihälä <johanna.raihala@gmail.com>
2021-02-12 14:19:29 +0100

    Add text to readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # meme-scraper
 
-read data from url
+read data from url,
+download pictures from a meme-examples page
+
+used libraries 'cheerio' for searching in a downloaded html-document
+and 'axios' for sending requests (to download) text (html) or binary (image) data
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // for creating folders, saving files on the harddrive
 const fs = require('fs');
-// for searching in a downloaded html document
 const cheerio = require('cheerio');
-// for sending requests (to download) text (html) or binary (image) data
 const axios = require('axios');
 
 // create a folder and wait until it exist
@@ -11,7 +9,7 @@ fs.mkdirSync('memes');
 // delcare a function that downloads a file and saves it to the harddrive
 const saveImage = (imageUrl) => {
   // given a url, extract the file name
-  // imagine urls are formatted like https://bblablalba.com/images/dog.jpg?width=300
+  // imagine urls are formatted like https://blablaa.com/images/dog.jpg?width=300
   // then the file name is everything after the last / and the last ? in the url
   const fileName = imageUrl.slice(
     imageUrl.lastIndexOf('/') + 1,
@@ -34,19 +32,18 @@ axios.get('https://memegen-link-examples-upleveled.netlify.app').then(
   (response) => {
     // status should be HTTP code 200 which means the connection was successful
     if (response.status === 200) {
-      // response from the server is in response.data - that is the html document we have just requested
+      // response from the server is in response.data - that is the html document just requested
       const html = response.data;
-      // now use cheerio to turn that html string into an object that can be searched, filtered, etc. - it's way more convenient to use cheerio than to your own searching in a huge string
+      // now use cheerio to turn that html string into an object that can be searched, filtered, etc.
       const $ = cheerio.load(html);
 
-      // now ask cheero (which is $) to return all images
+      // now ask cheerio (which is $) to return all images
       $('img').each((i, elem) => {
         // every image has a number, consider only 0 - 9, skip the rest
         if (i >= 10) return;
 
-        // for every image, read it's src attribute, i.e., the image file url
+        // for every image, read it's src attribute, i.e., the image file url and call previously defined 'saveImage'-function to download the image
         const imageUrl = $(elem).attr('src');
-        // now call the previously defined function to download the image
         saveImage(imageUrl);
       });
     }
